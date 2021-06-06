49de09adf2e392a117f1e08e43dc195fc8a04b10
Gustavo Carlini <gustavo.carlini@outlook.com>
2021-06-06 15:52:53 -0300

    Tratamento de dados loginrota e usuario

--- Servidor/api/controllers/LoginController.js
+++ Servidor/api/controllers/LoginController.js
@@ -1,10 +1,6 @@
-
 const database =  require('../models')
-
 const senhaHash = require('../estrategiaLogin/senhaHashController')
-
 const jwt = require('jsonwebtoken')
-
 
 function criaTokenJWT(login){
     const payload = {
@@ -13,18 +9,16 @@ function criaTokenJWT(login){
     const token = jwt.sign(payload, process.env.CHAVE_JWT)
     return token;
 }
-
 module.exports = {
     async listar(req,res){
         try{
             const login = await database.Login.findAll()
-            return res.status(200).json(login)
+            return res.status(200).json(loginnome(login))
         }catch(error){
             return res.status(400).json({erro:error.message})
         }
     },
     login: (req,res)=>{
-        console.log(req.body)
         const token = criaTokenJWT(req.body)
         res.set('Authorization', token)
         res.status(204).send();
@@ -36,7 +30,7 @@ module.exports = {
                 req.body.senha = await senhaHash.adiconaSenha(req)
                 console.log(req.body)
                 const login = await database.Login.create(req.body)
-                return res.status(201).json(login)
+                return res.status(201).json({id:login.id, senha:login.senha}) 
             }else{
                 throw new Error("Desculpe, mas nao foi possivel inserir um novo usuario!")
             }
@@ -50,7 +44,7 @@ module.exports = {
 
                 const login = await database.Login.findByPk(req.params.id)
                 await login.update(req.body)
-                res.status(200).json(login)
+                res.status(200).json({usuario_id:login.usuario_id, senha:login.senha})
             }else{
                 return res.status(400).json({erro:"Desculpe, mas nao foi possivel atualizar!"})
             }
@@ -59,4 +53,10 @@ module.exports = {
         }
     },
 }
-
+function loginnome(arr){
+    let loginlogin = [];
+    for(i=0;i<arr.length;i++){
+        loginlogin.push({id: arr[i].id, senha: arr[i].senha})
+    }
+    return loginlogin
+}
--- Servidor/api/routes/tipoRota.js
+++ Servidor/api/routes/tipoRota.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const tipoController = require("../controllers/TipoController")
 const middlewaresAutenticacao = require('../estrategiaLogin/middlewares-autenticacao')
+const passport = require('passport')
+
 
 router.get("/listar",tipoController.listarTipos)
 router.get("/buscar/:id",tipoController.listarTipo)
-router.post('/inserir',middlewaresAutenticacao.bearer,tipoController.inserirTipo)
+router.post('/inserir',passport.authenticate('bearer',{session:false}),tipoController.inserirTipo)
 router.put('/atualizar/:id',middlewaresAutenticacao.bearer,tipoController.atualizarTipo)
 router.delete("/deletar/:id",middlewaresAutenticacao.bearer,tipoController.deletarTipo)
 
