06ada94eb8d153244dc940dc5f6f0179b42438e1
Gustavo Carlini <gustavo.carlini@outlook.com>
2021-06-06 17:15:26 -0300

    Tratamento de dados Tipo e middleware

--- Servidor/api/routes/itemRota.js
+++ Servidor/api/routes/itemRota.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 const itemController = require("../controllers/ItemController")
 const middlewaresAutenticacao = require('../estrategiaLogin/middlewares-autenticacao')
+const passport = require('passport')
+
 
 router.get("/listar",itemController.listarItens)
 router.get("/buscar/:id",itemController.listarItem)
-router.post('/inserir',middlewaresAutenticacao.bearer,itemController.inserirItem)
-router.put('/atualizar/:id',middlewaresAutenticacao.bearer,itemController.atualizarItem)
-router.delete("/deletar/:id",middlewaresAutenticacao.bearer,itemController.deletarItem)
+router.post('/inserir',passport.authenticate('bearer',{session:false}),itemController.inserirItem)
+router.put('/atualizar/:id',passport.authenticate('bearer',{session:false}),itemController.atualizarItem)
+router.delete("/deletar/:id",passport.authenticate('bearer',{session:false}),itemController.deletarItem)
 
 module.exports = router
 
